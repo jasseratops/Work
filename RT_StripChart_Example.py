f350bc41c78422ca5dda0a6b1226365579cea348
jasseratops <mhalshehri92@gmail.com>
2018-04-04 16:39:32 -0500

    Differentiating between StripChart+Serial, and the matplotlib StripChart example.

--- RT_StripChart_Example.py
+++ RT_StripChart_Example.py
@@ -6,6 +6,8 @@
 
 
 import numpy as np
+from numpy import sin, cos, pi
+import time
 from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -20,7 +22,7 @@ class Scope(object):
         self.ydata = [0]
         self.line = Line2D(self.tdata, self.ydata)
         self.ax.add_line(self.line)
-        self.ax.set_ylim(-.1, 1.1)
+        self.ax.set_ylim(-1.1, 1.1)
         self.ax.set_xlim(0, self.maxt)
 
     def update(self, y):
@@ -41,15 +43,9 @@ class Scope(object):
 def emitter(p=0.03):
     'return a random value with probability p, else 0'
     while True:
-        v = np.random.rand(1)
-        if v > p:
-            yield 0.
-        else:
-            yield np.random.rand(1)
+        yield cos(time.time()*2*pi)
 
 # Fixing random state for reproducibility
-np.random.seed(19680801)
-
 
 fig, ax = plt.subplots()
 scope = Scope(ax)
